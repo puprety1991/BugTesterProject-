4f75d3821e93df62ded7bf8b80a0ca52fcabfde8
rimmaryspek <rimmateltaeva@gmail.com>
2023-03-25 22:33:23 -0400

    70% of common method id dome

--- src/test/java/runner/SmokeRunner.java
+++ src/test/java/runner/SmokeRunner.java
@@ -1,4 +1,28 @@
 package runner;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions
+        (
+                // features we use to provide the path of all the features files
+                features = "src/test/resources/features",
+                //glue means attached all the files
+                glue = "steps",
+                //when you set dryRun == true means its stop all the other steps
+                //it will execute only new scenarios
+                //dryRun =true
+                //when we set dryRun to false will execute all again
+                dryRun= true,
+                //to execute test cases from different sprint's like below
+                tags="@Smoke",
+                // to remove irrelevant info from concole you need to say monochrome to true
+                monochrome = true,
+                plugin = {"pretty"}
+        )
+
 public class SmokeRunner {
+
 }
