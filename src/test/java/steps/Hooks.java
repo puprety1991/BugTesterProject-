f4f785db1854040e8bfcc170bdee4bbf8fe3af17
rimmaryspek <rimmateltaeva@gmail.com>
2023-03-27 21:15:44 -0400

    chamges were added

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -1,4 +1,11 @@
 package steps;
 
-public class Hooks {
+import io.cucumber.java.Scenario;
+import org.junit.After;
+import org.junit.Before;
+import utils.CommonMethods;
+
+public class Hooks extends CommonMethods {
+
+
 }
