5cb251869589349ef88ff3ba8fab5942ec694217
ZorahY <zorah.yavaveva@gmail.com>
2023-03-27 21:10:53 -0400

    Common method was added. Hooks were completed

--- src/test/java/utils/Constants.java
+++ src/test/java/utils/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
     public static final int IMPLICIT_WAIT = 10;
 
     public static final String TESTDATA_FILEPATH=
-            System.getProperty("user.dir") +"src/test/resources/testdata/testData.xlsx";
+            System.getProperty("user.dir") +"src/test/resources/testdata/testData1.xlsx";
 
     public static final String SCREENSHOT_FILEPATH= System.getProperty("user.dir")+"/screenshot/";
 
